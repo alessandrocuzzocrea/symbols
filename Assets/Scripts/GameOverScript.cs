5dc3994d799c7861a053dcb694cfc799a2897207
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2020-12-06 15:40:18 +0900

    Save best score

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameOverScript : MonoBehaviour
@@ -7,8 +8,35 @@ public class GameOverScript : MonoBehaviour
     [SerializeField]
     GameObject gameOverUIRoot;
 
+    [SerializeField]
+    TextMeshProUGUI scoreText;
+
+    [SerializeField]
+    TextMeshProUGUI bestText;
+
+    private readonly string BestScoreKey = "BestScore";
+
     public void Show(int score)
     {
+        SaveBestScore(score);
+
+        scoreText.text = score.ToString();
+        bestText.text  = LoadBestScore().ToString();
+
         gameOverUIRoot.SetActive(true);
+    }
+
+    private void SaveBestScore(int newBest)
+    {
+        if (newBest > LoadBestScore())
+        {
+            PlayerPrefs.SetInt(BestScoreKey, newBest);
+            PlayerPrefs.Save();
+        }
+    }
+
+    private int LoadBestScore()
+    {
+        return PlayerPrefs.GetInt(BestScoreKey, 0);
     }
 }
