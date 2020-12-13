e6652faecd6c6af336a80ca35797cfa1d1095fa0
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2020-12-13 12:43:43 +0900

    LockOrientation

--- Assets/Scripts/AppScript.cs
+++ Assets/Scripts/AppScript.cs
@@ -6,6 +6,11 @@ public class AppScript : MonoBehaviour
     private void Awake()
     {
         SetTargetFrameRate();
+
+        if (IsIphone())
+        {
+            LockOrientation();
+        }
     }
 
     private void OnEnable()
@@ -26,6 +31,23 @@ public class AppScript : MonoBehaviour
     private void SetTargetFrameRate()
     {
         Application.targetFrameRate = 60;
+    }
+
+    private bool IsIphone()
+    {
+        if (SystemInfo.deviceModel.Contains("iPhone"))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void LockOrientation()
+    {
+        Screen.autorotateToPortrait = false;
+        Screen.autorotateToPortraitUpsideDown = false;
+        Screen.orientation = ScreenOrientation.Portrait;
     }
 
 #if DEVELOPMENT_BUILD || UNITY_EDITOR
