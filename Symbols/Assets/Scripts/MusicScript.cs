4230cd1f710860b74a745b186dfb5e252e33ea61
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2020-12-18 00:24:26 +0900

    Music

---
+++ Symbols/Assets/Scripts/MusicScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class MusicScript : MonoBehaviour
+{
+    static bool initialized = false;
+
+    AudioSource music;
+
+    private void OnEnable()
+    {
+        if (initialized)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            music = GetComponent<AudioSource>();
+            DontDestroyOnLoad(this.gameObject);
+            initialized = true;
+        }
+
+        //PlayMusic();
+    }
+
+    void PlayMusic()
+    {
+        if (!music.isPlaying)
+        {
+            music.Play();
+        }
+    }
+}
